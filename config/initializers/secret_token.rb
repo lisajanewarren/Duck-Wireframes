e9fa106404a6ef4c3984a688151958df801c7f79
Lisa Warren <lisajanewarren@gmail.com>
2011-06-30 11:27:26 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DuckWireframes::Application.config.secret_token = 'c02213b3a170d4a2451da6223c3c9c69e2aa38ecf21637476f62126adc0090e57a5bd44835187284335748e648a2c6ffbae53d883655a996f17c7c0bf7982c72'
